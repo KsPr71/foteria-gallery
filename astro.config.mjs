402725c4d7ae0ef5f34a4a1cc10b337c9e65cce4
KsPr71 <jorgealejandrocasaresdelgado@gmail.com>
2024-02-06 20:35:04 -0500

    creacion del menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,11 +16,29 @@ export default defineConfig({
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Newborns', link: '/catalogo/newborn/' },
 						{ label: 'Mes a mes', link: '/catalogo/meses/' },
+						{ label: 'Mi primer añito', link: '/catalogo/first-year/' },
 					],
 				},
 				{
 					label: 'Fotografías temáticas',
-					autogenerate: { directory: 'reference' },
+					items: [
+						{label: 'Princesas de Disney', link: '/catalogo/newborn/'},
+						{label: 'Mi futura profesión', link: '/catalogo/newborn/'},
+						{label: 'Superhéroes', link: '/catalogo/newborn/'},
+						{label: 'Junto a mis mascotas', link: '/catalogo/newborn/'}
+
+				],
+				},
+				{
+					label: 'Especiales de temporada',
+					items: [
+						{label: 'Día del amor', link: '/catalogo/newborn/'},
+						{label: 'Para mamá', link: '/catalogo/newborn/'},
+						{label: 'Con mi papi', link: '/catalogo/newborn/'},
+						{label: 'Halloween', link: '/catalogo/newborn/'},
+						{label: 'Navidad', link: '/catalogo/newborn/'}
+
+				],
 				},
 			],
 		}),
